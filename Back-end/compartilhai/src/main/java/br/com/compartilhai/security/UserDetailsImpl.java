88c51da9aa566cf1258938d36a01f09ec7fa25eb
JuCSMartin <julianacmsantos@gmail.com>
2021-07-12 10:15:16 -0300

    Package Security -> BasicSecurityConfig e UserDetailsServiceImpl

---
+++ Back-end/compartilhai/src/main/java/br/com/compartilhai/security/UserDetailsImpl.java
@@ -0,0 +1,5 @@
+package br.com.compartilhai.security;
+
+public class UserDetailsImpl {
+
+}
---
+++ Back-end/compartilhai/src/main/java/br/com/compartilhai/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package br.com.compartilhai.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.compartilhai.model.Usuario;
+import br.com.compartilhai.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		Optional <Usuario> user = usuarioRepository.findByEmail(email);
+		user.orElseThrow(() -> new UsernameNotFoundException(email + " não encontrado."));
+		
+		return user.map(UserDetailsImpl::new).get();
+	}
+}

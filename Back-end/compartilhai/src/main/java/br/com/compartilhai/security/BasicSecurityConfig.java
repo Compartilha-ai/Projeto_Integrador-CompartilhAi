6bd2817239eb3f9d41bd0376cd05a739336a7e29
Vinicius Pacheco <pachecovinicius18@gmail.com>
2021-08-09 11:53:25 -0300

    Alteração na BasicSecurity

--- Back-end/compartilhai/src/main/java/br/com/compartilhai/security/BasicSecurityConfig.java
+++ Back-end/compartilhai/src/main/java/br/com/compartilhai/security/BasicSecurityConfig.java
@@ -35,6 +35,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/usuarios/{id}").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/produto").permitAll()
